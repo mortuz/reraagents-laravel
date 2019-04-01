cf2ca229a38b3a4b83da5b8f613476fcadd7fde4
mortuz <mortuzalam@gmail.com>
2019-04-01 09:31:37 -0500

    remove api route

--- routes/api.php
+++ routes/api.php
@@ -15,8 +15,8 @@ use Illuminate\Http\Request;
 |
 */
 
-Route::middleware('auth:api')->get('/user', function (Request $request) {
-    return $request->user();
-});
+//Route::middleware('auth:api')->get('/user', function (Request $request) {
+//    return $request->user();
+//});
 
 Route::get('/cities', 'API\CitiesController@citiesFromState')->name('get.cities');
