4bd43fd391a103a1de0d4f47a3307de94b9e2947
mortuz <mortuzalam@gmail.com>
2019-04-16 10:47:09 +0530

    fix password on signup

--- app/Http/Controllers/API/AuthController.php
+++ app/Http/Controllers/API/AuthController.php
@@ -25,7 +25,7 @@ class AuthController extends Controller
         $user = User::create([
             'name' => $request->name,
             'email' => $request->email,
-            'password' => $request->password,
+            'password' => bcrypt($request->password),
             'mobile' => $request->mobile,
             'role' => '5'
         ]);
