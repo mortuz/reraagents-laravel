811f69ef5a85c959ea96b1ac037a427a5e543f9f
dhar1993 <38814523+dhar1993@users.noreply.github.com>
2019-05-05 13:21:36 +0530

    privacy fixed

--- resources/views/frontend/privacy-policy.blade.php
+++ resources/views/frontend/privacy-policy.blade.php
@@ -5,31 +5,32 @@
         <div class="container">
             <div class="partners">
                 <p><strong></strong></p>
-<h5><strong>Reraagents.in website is owned and operated by CHARANETRA SPACES PRIVATE LIMITED.</strong></h5><br>
-<ul>
-<li>Charanetra Spaces Private Limited has created this privacy statement in order to demonstrate our commitment to our user's privacy.</li><br>
-<li>We may use IP address to help diagnose problems with our server, and to administer our Web site. Your IP address may be also used to gather broad demographic information.</li><br>
-<li>in contains links to other websites. Charanetra Spaces Private Limited. is not responsible for the privacy practices or the content of such Web sites.</li><br>
-<li>We may use a third party ad company to display ads on Reraagents.in (e.g. Google Adsense). These ads may contain cookies. While we may use cookies in other parts of our Web site, cookies received with banner ads are collected by our ad company, and we do not have access to this information.</li><br>
-<li>Certifiable information is collected from users and/or registered members of Reraagents.in which includes, but may not be limited to
-<ul>
-<li>Email address.</li>
-<li>Contact person name.</li>
-<li>User-created password.</li>
-<li>Mailing address.</li>
-<li>Pin code.</li>
-<li>PAN Number</li>
-<li>RERA Certificate &amp; Certificate ID number</li>
-<li>Telephone number or mobile number or other contact number</li>
-</ul>
-</li><br>
-<li>Except where you are explicitly informed, Charanetra Spaces Private Limited. does not sell, rent, share, trade or give away any of your personal information.</li>
-<li>in may tie-up with partners to bring you various services. However, we will never share your personal information, without your explicitly permission. Still, we encourage you to review the privacy policy of the partner for questions about their use, any personally identifiable information that you may separately submit to such partner.</li>
-<li>in may tie-up with partners to bring you various services. However, we will never share your personal information, without your explicitly permission. Still, we encourage you to review the privacy policy of the partner for questions about their use, any personally identifiable information that you may separately submit to such partner.</li>
-<li>If we change our privacy policy, we will prominently post a link to those changes on our Homepage so you are always aware of what information we collect, how we use it, and under what circumstances, if any, we disclose it. If at any point we decide to use personally identifiable information in a manner materially different from that stated at the time it was collected, we will notify you by way of an email. We will give you a choice as to whether or not we can use information in this different manner and act accordingly.</li>
-<li>Quality is of paramount importance for our business, Charanetra Spaces Private Limited will periodically verify the authenticity, availability and integrity of the content posted by the seller. If it is found that the content is not as per quality guideline or member is not available to answer the call, Reraagents.in reserves the right to put the content on hold until the suggested resolution is verified. Repeated abuse of quality guidelines can lead to cancellation of product / services. In case of repeated abuse the amount paid will be forfeited and no refund request will be entertained.&nbsp;<br /> <br /> Charanetra Spaces Private Limited reserves the right to monitor / record calls between buyers and sellers for quality checks and monitoring purposes.&nbsp;<br /> If you have questions or concerns regarding this statement, please email to support@reraagents.in</li>
-</ul>
-<p>&nbsp;</p>
+                <h5><strong>Reraagents.in website is owned and operated by CHARANETRA SPACES PRIVATE LIMITED.</strong></h5><br>
+                <ul>
+                    <li class="mb-3">Charanetra Spaces Private Limited has created this privacy statement in order to demonstrate our commitment to our user's privacy.</li>
+                    <li class="mb-3">We may use IP address to help diagnose problems with our server, and to administer our Web site. Your IP address may be also used to gather broad demographic information.</li>
+                    <li class="mb-3">in contains links to other websites. Charanetra Spaces Private Limited. is not responsible for the privacy practices or the content of such Web sites.</li>
+                    <li class="mb-3">We may use a third party ad company to display ads on Reraagents.in (e.g. Google Adsense). These ads may contain cookies. While we may use cookies in other parts of our Web site, cookies received with banner ads are collected by our ad company, and we do not have access to this information.</li>
+                    <li class="mb-3">Certifiable information is collected from users and/or registered members of Reraagents.in which includes, but may not be limited to
+                        <ul>
+                            <li>Email address.</li>
+                            <li>Contact person name.</li>
+                            <li>User-created password.</li>
+                            <li>Mailing address.</li>
+                            <li>Pin code.</li>
+                            <li>PAN Number</li>
+                            <li>RERA Certificate &amp; Certificate ID number</li>
+                            <li>Telephone number or mobile number or other contact number</li>
+                        </ul>
+                    </li>
+
+                    <li class="mb-3">Except where you are explicitly informed, Charanetra Spaces Private Limited. does not sell, rent, share, trade or give away any of your personal information.</li>
+                    <li class="mb-3">in may tie-up with partners to bring you various services. However, we will never share your personal information, without your explicitly permission. Still, we encourage you to review the privacy policy of the partner for questions about their use, any personally identifiable information that you may separately submit to such partner.</li>
+                    <li class="mb-3">in may tie-up with partners to bring you various services. However, we will never share your personal information, without your explicitly permission. Still, we encourage you to review the privacy policy of the partner for questions about their use, any personally identifiable information that you may separately submit to such partner.</li>
+                    <li class="mb-3">If we change our privacy policy, we will prominently post a link to those changes on our Homepage so you are always aware of what information we collect, how we use it, and under what circumstances, if any, we disclose it. If at any point we decide to use personally identifiable information in a manner materially different from that stated at the time it was collected, we will notify you by way of an email. We will give you a choice as to whether or not we can use information in this different manner and act accordingly.</li>
+                    <li>Quality is of paramount importance for our business, Charanetra Spaces Private Limited will periodically verify the authenticity, availability and integrity of the content posted by the seller. If it is found that the content is not as per quality guideline or member is not available to answer the call, Reraagents.in reserves the right to put the content on hold until the suggested resolution is verified. Repeated abuse of quality guidelines can lead to cancellation of product / services. In case of repeated abuse the amount paid will be forfeited and no refund request will be entertained.&nbsp;<br /> <br /> Charanetra Spaces Private Limited reserves the right to monitor / record calls between buyers and sellers for quality checks and monitoring purposes.&nbsp;<br /> If you have questions or concerns regarding this statement, please email to support@reraagents.in</li>
+                </ul>
+                <p>&nbsp;</p>
             </div>
         </div>
     </section>
