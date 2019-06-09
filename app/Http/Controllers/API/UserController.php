0e60b1beeb6a33078e815fcd0d26dd6661320990
mortuz <mortuzalam@gmail.com>
2019-06-09 19:14:38 +0530

    fix ad

--- app/Http/Controllers/API/UserController.php
+++ app/Http/Controllers/API/UserController.php
@@ -29,11 +29,11 @@ class UserController extends Controller
         $ads = [];
         $ad = null;
         // look for city ads
-        $ad = Advertisement::where('city_id', $request->user()->city_id)->latest()->first();
+        $ad = Advertisement::where('city_id', $request->user()->agent->city_id)->latest()->first();
         
         // look for state ads
         if (!$ad) {
-            $ad = Advertisement::where('state_id', $request->user()->state_id)->where('city_id', '')->latest()->first();
+            $ad = Advertisement::where('state_id', $request->user()->agent->state_id)->where('city_id')->latest()->first();
         }
 
         // look for any ads
