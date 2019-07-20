76678f3969c79866126398ba310baec73c948601
mortuz <mortuzalam@gmail.com>
2019-07-20 11:21:36 +0530

    ui update

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -138,28 +138,34 @@
                                 <div class="card ml-1 mr-1 bx-shadow mt-3">
                                     <div class="row no-gutters">
                                         <div class="col-md-4">
-                                            <div class="premium-img" style="background-image: url('{{ $property->images ? $property->images[0] : 'https://via.placeholder.com/250x200?text=N/A' }} ')"></div>
+                                            <div class="premium-img" style="background-image: url('{{ $property->images ? $property->images[0] : asset('img/na.jpeg') }} ')"></div>
                                         </div>
                                         <div class="col-md-8">
                                             <div class="card-body">
-                                                <p class="card-text">{{ $property->state->name }} >> <span class="color-dark">{{ $property->city ? $property->city->name : null }}</span> 
+                                                <p class="card-text">{{ $property->state->name }} >> <span>{{ $property->city ? $property->city->name : null }}</span>
                                                     @if ($property->propertytypes->first())
-                                                        >> <span class="color-dark">{{ $property->propertytypes->first()->type }}</span>                                                
+                                                        >> <span>{{ $property->propertytypes->first()->type }}</span>                                                
                                                     @endif
                                                 </p>
                                                 <h5 class="card-title card-t1">{{ $property->raw_data->details }}</h5>
-                                                {{-- <p class="card-text">{{ $property->areas->first() ? $property->areas->first()->area : '' }}</p>
-                                                <p class="card-text">{{ $property->prices->first() ? $property->prices->first()->price : '' }}</p> --}}
 
-                                                @if($property->areas->first()) <p class="card-text">{{$property->areas->first()->area}}</p> @endif
-                                                @if($property->landmarks->first()) <p class="card-text">{{$property->landmarks->first()->name}}</p> @endif
-                                                @if($property->prices->first()) <p class="card-text"><i class="fa fa-inr" aria-hidden="true"></i> {{$property->prices->first()->price}}</p> @endif
+                                                <p class="card-text"><i class="fa fa-map-marker" aria-hidden="true"></i>&nbsp;
+                                                    @if($property->areas->first()) 
+                                                        {{$property->areas->first()->area}}
+                                                    @endif
+                                                    @if($property->landmarks->first()) 
+                                                        {{$property->landmarks->first()->name}} 
+                                                    @endif
+                                                </p>
+                                                @if($property->prices->first()) 
+                                                    <p class="card-text"><i class="fa fa-inr" aria-hidden="true"></i> {{$property->prices->first()->price}}</p>
+                                                @endif
 
                                                 <p class="card-text my-3"><span class="card-text">Property Id: 
                                                     <span class="color-red" style="font-weight:bold;">{{ $property->id }}</span> , <span class="color-grey" style="font-weight:bold;">{{ $property->updated_at->format('Y-m-d') }}</span></span>
                                                 </p>
 
-                                                <a class="btn btn-primary mt-3 font-weight-normal" href="{{ route('show.property', ['id' => $property->id]) }}" target="_blank">
+                                                <a class="btn btn-primary mt-3 font-weight-normal" style="font-size: 14px" href="{{ route('show.property', ['id' => $property->id]) }}" target="_blank">
                                                     More Information
                                                 </a>
 
@@ -189,10 +195,10 @@
                                             @if($property->landmarks->first()) 
                                                 {{$property->landmarks->first()->name}} 
                                             @endif
-                                            </p>
+                                        </p>
                                         @if($property->prices->first()) <p class="card-text"><i class="fa fa-inr" aria-hidden="true"></i>&nbsp; {{$property->prices->first()->price}}</p> @endif
                                         {{-- <p class="card-text"></p> --}}
-                                        <a class="btn btn-primary mt-3 font-weight-normal" href="{{ route('show.property', ['id' => $property->id]) }}" target="_blank">
+                                        <a class="btn btn-primary mt-3 font-weight-normal" style="font-size: 14px" href="{{ route('show.property', ['id' => $property->id]) }}" target="_blank">
                                             More Information
                                         </a>
                                     </div>
@@ -214,7 +220,7 @@
                             <a href="{{ route('agent.details', ['id' => $agent->id]) }}">
                                 <div class="card ml-1 mr-1 bx-shadow mt-3">
                                     <div class="card-body">
-                                        <h5>{{ $agent->user->name }}</h5>
+                                        <h5 style="font-size: 16px">{{ $agent->user->name }}</h5>
                                         <p>
                                             {{ $agent->state->name }} <br>
                                             {{ $agent->city->name }} <br>
@@ -228,7 +234,7 @@
                                             @endif
                                         </p>
 
-                                        <h6>{{ $agent->user->mobile }}</h6>
+                                        <h6 style="font-size: 14px">{{ $agent->user->mobile }}</h6>
 
                                     </div>
                                 </div>
