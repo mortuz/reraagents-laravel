2911e6730321ab38427338cf65d9ed6e0a73f88d
mortuz <mortuzalam@gmail.com>
2019-07-22 23:38:30 +0530

    fix

--- resources/views/call-records/index.blade.php
+++ resources/views/call-records/index.blade.php
@@ -36,7 +36,7 @@
                     <td> {{ $callRecord->city->name }} </td>
                     <td>
                       @foreach ($callRecord->comments as $comment)
-                          <p>{!! $comment->comment !!}</p>
+                          <p>{!! nl2br(e($comment->comment)) !!}</p>
                           <hr>
                       @endforeach
                     </td>
